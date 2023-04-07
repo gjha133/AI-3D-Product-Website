8113a9781a5458049d69fc18f43794ce917e57f2
Gautam Jha <gjha133@gmail.com>
2023-04-07 12:41:38 +0530

    deploy backend

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -3,7 +3,7 @@ const config = {
     backendUrl: "http://localhost:5000/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+    backendUrl: "https://ai-3d-product-server.onrender.com/api/v1/dalle",
   },
 };
 
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -55,7 +55,7 @@ const Customizer = () => {
 
     try {
       setGeneratingImg(true)
-      const response = await fetch('http://localhost:5000/api/v1/dalle', {
+      const response = await fetch(config.production.backendUrl, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
