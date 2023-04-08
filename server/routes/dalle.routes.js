06e0fe20cd36a1ce9ab058c493db7b17f1d99300
Gautam Jha <53974342+gjha133@users.noreply.github.com>
2023-04-08 13:36:47 +0530

    Update dalle.routes.js

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -39,11 +39,5 @@ router.post('/', async (req, res) => {
 
 export default router
 
-// const response = await openai.createImage({
-//   prompt: "A cute baby sea otter",
-//   n: 2,
-//   size: "1024x1024",
-// });
-
 
 
